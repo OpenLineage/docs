55652ab564e854fd25f7a94bc90643a2740a5e15
Michael Robinson <merobi@gmail.com>
2023-01-18 14:04:21 -0500

    add lunr search plugin to docs site
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,6 +48,8 @@ const config = {
     ],
   ],
 
+  plugins: [require.resolve('docusaurus-lunr-search')],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
