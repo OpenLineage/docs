219523a3fdc8e043ad02b878b18bb54888a0d263
Ross Turk <ross@rossturk.com>
2023-03-28 23:30:39 -0500

    update layout and add more descriptive text
    Signed-off-by: Ross Turk <ross@rossturk.com>

--- src/components/addevent.tsx
+++ src/components/addevent.tsx
@@ -30,7 +30,7 @@ import React from "react"
 export default function () {
     return (
         <>
-        <div style={{maxWidth: "400px", height:"500px"}}>
+        <div style={{maxWidth: "100%", height:"500px", marginBottom:"2rem"}}>
             <iframe
                 src={"https://www.addevent.com/calendar/pP575215/embed-events/?frame=true&include_atc=true&include_stc=true&include_desc=false&include_timezone=false&include_timezone_select=true&include_location=false&include_organizer=false&include_countdown=false&default_view=upcoming&stayonpage=false&events_max=20&include_monthpick=true&datetimeformat=1&datelanguage=en_US"}
                 frameBorder="false"
