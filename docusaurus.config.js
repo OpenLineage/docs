30dbbc21d1c78b500ca3c8006fa1c2d7e16c3643
Ross Turk <ross@rossturk.com>
2023-03-30 17:09:33 -0500

    Small design tweaks and fixes (#138)
    * disable theme switcher
    
    Signed-off-by: Ross Turk <ross@rossturk.com>
    
    * fix vertical-align on github external link
    
    Signed-off-by: Ross Turk <ross@rossturk.com>
    
    * fix link from front page getting started
    
    Signed-off-by: Ross Turk <ross@rossturk.com>
    
    * box the content pages with left/right margin
    
    Signed-off-by: Ross Turk <ross@rossturk.com>
    
    ---------
    
    Signed-off-by: Ross Turk <ross@rossturk.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -133,6 +133,11 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: true,
+        respectPrefersColorScheme: false,
+      },  
     }),
 };
 
